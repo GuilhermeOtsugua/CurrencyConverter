dd996a0e23b96344ca392b8261e84491f444ed71
GuilhermeOtsugua <guilhermebartolis@gmail.com>
2018-07-04 22:34:25 -0300

    +Opções e Temas

--- app/screens/CurrencyList.js
+++ app/screens/CurrencyList.js
@@ -7,29 +7,29 @@ import currencies from '../data/currencies';
 const TEMP_CURRENT_CURRENCY = 'CAD';
 
 class CurrencyList extends Component {
-    handlePress = () => {
-        console.log('row press');
-    };
+  handlePress = () => {
+    console.log('row press');
+  };
 
-    render() {
-        return (
-            <View style={{ paddingTop: 20 }}>
-                <StatusBar translucent={false} barStyle="light-content" />
-                <FlatList
-                    data={currencies}
-                    renderItem={({ item }) => (
-                        <ListItem
-                            text={item}
-                            selected={item === TEMP_CURRENT_CURRENCY}
-                            onPress={this.handlePress}
-                        />
-                    )}
-                    keyExtractor={item => item}
-                    ItemSeparatorComponent={Separator}
-                />
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View>
+        <StatusBar translucent={false} barStyle="light-content" />
+        <FlatList
+          data={currencies}
+          renderItem={({ item }) => (
+            <ListItem
+              text={item}
+              selected={item === TEMP_CURRENT_CURRENCY}
+              onPress={this.handlePress}
+            />
+          )}
+          keyExtractor={item => item}
+          ItemSeparatorComponent={Separator}
+        />
+      </View>
+    );
+  }
 }
 
 export default CurrencyList;
